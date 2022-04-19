baf3d62992a7f0edbd8b3322832014c152aaacd2
jherry-Ponce <79391914+jherry-Ponce@users.noreply.github.com>
2022-04-18 19:28:56 -0500

    logo

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -7,7 +7,8 @@
             <figure class="md:w-full  mt-4 md:px-20 md:py-20 md:bg-red-100  md:shadow-md hidden md:block">
               <a href="/">
                 {{-- DE Aqui sale el logo psara todo --}}
-                <img class="shadow-xl rounded-sm" src="" alt="logo">
+               {{--  <img class="shadow-xl rounded-sm" src="" alt="logo"> --}}
+                <iframe class="shadow-xl rounded-sm" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdianaorozcograo%2Fphotos%2Fa.224898151664290%2F885168632303902%2F&show_text=true&width=500" width="500" height="534" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
              </a>
             </figure>
      
